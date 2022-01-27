2e7b1e4cd79902b2ac86f2d7be6b3473152e138a
SandhyaRavindran96 <sandhyaravindran96@gmail.com>
2022-01-26 17:53:52 -0800

    Delete run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-python src/most_active_cookie.py --logfile src/cookie_log.csv --date 2018-12-07
-python src/most_active_cookie.py --logfile src/cookie_log.csv --date 2018-12-08
-python src/most_active_cookie.py --logfile src/cookie_log.csv --date 2018-12-09
